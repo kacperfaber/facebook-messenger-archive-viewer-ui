4239409b963c79e6b203a544b1061414f31e8dba
Kacper Faber <kacperf1234@gmail.com>
2023-11-14 21:22:02 +0100

    Created Layout component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 
 <script setup lang="ts">
 
+import Layout from "@/components/layout/Layout.vue";
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <Layout></Layout>
 </template>
---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import AppToolbar from "@/components/layout/toolbar/AppToolbar.vue";
+</script>
+
+<template>
+<div id="app_layout">
+  <div id="app_toolbar">
+    <AppToolbar/>
+  </div>
+
+  <div id="app_router_outlet">
+    <RouterView/>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+#app_layout {
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+#app_toolbar {
+  width: 100vw;
+  height: 7vh;
+}
+
+#app_router_outlet {
+  width: 100vw;
+  height: 93vh;
+}
+</style>
