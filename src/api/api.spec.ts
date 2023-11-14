33a89e0fdf85eca85c09fa73c3753c6b09033441
Kacper Faber <kacperf1234@gmail.com>
2023-11-14 17:52:49 +0100

    Created IApi, ProdApi, DevApi

--- src/api/api.spec.ts
+++ src/api/api.spec.ts
@@ -1,60 +1,60 @@
 import {describe, expect, test, vi} from "vitest";
-import axios from "axios";
-import Api from "@/api/api";
-import type {PaginationApiModel} from "@/api/pagination";
+import {ProdApi} from "@/api/api";
 import type {MessageApiModel} from "@/api/message/message";
-
-
+import type {PaginationApiModel} from "@/api/pagination";
+import axios from "axios";
 
 describe("api", () => {
-    describe("getAllMessages", () => {
-        const defaultMessagesResult: PaginationApiModel<MessageApiModel> = {
-            total: 1,
-            page: 0,
-            per_page: 1,
-            items: [
-                {
-                    type: "generic",
-                    attachments: [],
-                    id: 0,
-                    sender_name: "Kacper Faber",
-                    sent_at: new Date(2023, 1, 1, 12, 0, 0),
-                    content: "Hello World!",
-                    call_duration: undefined
-                }
-            ]
-        };
+    describe("ProdApi", () => {
+        describe("getAllMessages", () => {
+            const defaultMessagesResult: PaginationApiModel<MessageApiModel> = {
+                total: 1,
+                page: 0,
+                per_page: 1,
+                items: [
+                    {
+                        type: "generic",
+                        attachments: [],
+                        id: 0,
+                        sender_name: "Kacper Faber",
+                        sent_at: new Date(2023, 1, 1, 12, 0, 0),
+                        content: "Hello World!",
+                        call_duration: undefined
+                    }
+                ]
+            };
 
-        const defaultResponse = {
-            data: defaultMessagesResult
-        }
+            const defaultResponse = {
+                data: defaultMessagesResult
+            }
 
-        test("does not throw", ()=>{
-            vi.spyOn(axios, "get").mockResolvedValueOnce(defaultResponse);
-            expect(() => Api.getAllMessages(0)).not.throws();
-        });
+            test("does not throw", ()=>{
+                vi.spyOn(axios, "get").mockResolvedValueOnce(defaultResponse);
+                expect(() => new ProdApi().getAllMessages(0)).not.throws();
+            });
 
-        test("returns expected items length", async () => {
-            vi.spyOn(axios, "get").mockResolvedValueOnce(defaultResponse);
-            expect((await Api.getAllMessages(0)).length).toBe(1);
-        });
+            test("returns expected items length", async () => {
+                vi.spyOn(axios, "get").mockResolvedValueOnce(defaultResponse);
+                expect((await new ProdApi().getAllMessages(0)).length).toBe(1);
+            });
 
-        test("returns expected items length", async () => {
-            vi.spyOn(axios, "get").mockResolvedValueOnce(defaultResponse);
-            expect((await Api.getAllMessages(0)).length).toBe(1);
-        });
+            test("returns expected items length", async () => {
+                vi.spyOn(axios, "get").mockResolvedValueOnce(defaultResponse);
+                expect((await new ProdApi().getAllMessages(0)).length).toBe(1);
+            });
 
-        test("returns expected item data", async () => {
-            vi.spyOn(axios, "get").mockResolvedValueOnce(defaultResponse);
-            const result = await Api.getAllMessages(0);
-            const item = result[0];
-            expect(item.type).toBe("generic");
-            expect(item.id).toBe(0)
-            expect(item.attachments.length).toBe(0);
-            expect(item.senderName).toBe("Kacper Faber");
-            expect(item.sentAt).toStrictEqual(new Date(2023, 1, 1, 12, 0, 0));
-            expect(item.content).toBe("Hello World!");
-            expect(item.callDuration).toBe(undefined);
+            test("returns expected item data", async () => {
+                vi.spyOn(axios, "get").mockResolvedValueOnce(defaultResponse);
+                const result = await new ProdApi().getAllMessages(0);
+                const item = result[0];
+                expect(item.type).toBe("generic");
+                expect(item.id).toBe(0)
+                expect(item.attachments.length).toBe(0);
+                expect(item.senderName).toBe("Kacper Faber");
+                expect(item.sentAt).toStrictEqual(new Date(2023, 1, 1, 12, 0, 0));
+                expect(item.content).toBe("Hello World!");
+                expect(item.callDuration).toBe(undefined);
+            });
         });
     });
 });
--- src/api/api.ts
+++ src/api/api.ts
@@ -4,14 +4,31 @@ import MessageConverter from "@/api/message/messageConverter";
 import type {Thread} from "@/api/thread/thread";
 import ThreadConverter from "@/api/thread/threadConverter";
 
-export default {
-    getAllMessages(threadId: number): Promise<Message[]> {
+export interface IApi {
+    getAllMessages(threadId: number): Promise<Message[]>;
+    getAllThreads(limit: number, page: number): Promise<Thread[]>;
+}
+
+export class ProdApi implements IApi {
+    public async getAllMessages(threadId: number): Promise<Message[]> {
         return axios.get(`/conversation/${threadId}/messages/desc`)
             .then(r => MessageConverter.convertMessages(r.data.items));
-    },
+    }
 
-    getAllThreads(limit: number = 0, page: number = 0): Promise<Thread[]> {
+    public async getAllThreads(limit: number = 0, page: number = 0): Promise<Thread[]> {
         return axios.get(`/conversation/all?limit=${limit}&page=${page}`)
             .then(r => ThreadConverter.convertThreads(r.data.items));
     }
 }
+
+export class DevApi implements IApi{
+    public async getAllMessages(threadId: number): Promise<Message[]> {
+        return [];
+    }
+
+    public async getAllThreads(limit: number, page: number): Promise<Thread[]> {
+        return Promise.resolve([]);
+    }
+}
+
+export const Api: IApi = import.meta.env.DEV ? new DevApi() : new ProdApi();
