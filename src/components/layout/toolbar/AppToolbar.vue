c0634871d9505533dcd75606c77fb8269ad8c0cf
Kacper Faber <kacperf1234@gmail.com>
2023-11-14 21:12:23 +0100

    Created simple AppToolbar. It has nothing actually.

---
+++ src/components/layout/toolbar/AppToolbar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Menubar from "primevue/menubar";
+</script>
+
+<template>
+<div id="app_toolbar">
+
+</div>
+</template>
+
+<style scoped lang="scss">
+#app_toolbar {
+  border-bottom: 1px rgba(255, 255, 255, 0.25) solid;
+  height: 7vh;
+  width: 100vw;
+  //
+  //position: fixed;
+  //top: 0;
+  //left: 0;
+  //
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
