edb0318b7f13979cf8d19727454ae2e169d34487
Kacper Faber <kacperf1234@gmail.com>
2023-11-15 12:37:07 +0100

    Created default route / to MainView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import MainView from "@/views/MainView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            component: MainView
+        }
+    ]
 })
 
 export default router
