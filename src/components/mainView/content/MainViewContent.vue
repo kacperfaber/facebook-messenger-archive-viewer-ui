187f9b3bcdd48977725970a0f98dc26b3538309d
Kacper Faber <kacperf1234@gmail.com>
2023-11-16 00:27:10 +0100

    MainViewContent - created no thread selected

--- src/components/mainView/content/MainViewContent.vue
+++ src/components/mainView/content/MainViewContent.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
-import MessageList from "@/components/message/MessageList.vue";
+import ScrollableMessageContainer from "@/components/message/ScrollableMessageContainer.vue";
+import {AppStore} from "@/store/appStore";
 </script>
 
 <template>
-
+  <div v-if="!AppStore.selectedThread" class="no-thread-selected">
+    <h5>No thread selected.</h5>
+    <p>Choose one</p>
+  </div>
+<ScrollableMessageContainer v-if="AppStore.selectedThread" :thread-id="AppStore.selectedThread?.id"/>
 </template>
 
 <style scoped lang="scss">
+.no-thread-selected {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
+  height: 100%;
+  width: 100%;
+}
 </style>
