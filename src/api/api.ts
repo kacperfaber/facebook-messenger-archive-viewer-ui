b6a3084df2639d2345e47d61f630e067edd1e36c
Kacper Faber <kacperf1234@gmail.com>
2023-11-15 23:00:23 +0100

    Api.ts -> get messages paginated.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,27 @@
-import type {Message} from "@/api/message/message";
+import type {Message, MessageApiModel} from "@/api/message/message";
 import axios from "axios";
 import MessageConverter from "@/api/message/messageConverter";
 import type {Thread} from "@/api/thread/thread";
 import ThreadConverter from "@/api/thread/threadConverter";
+import type {PaginationApiModel} from "@/api/pagination";
+
+export type Pagination<T> = {
+    items: T[];
+    page: number;
+    perPage: number;
+    total: number;
+}
 
 export interface IApi {
     getAllMessages(threadId: number): Promise<Message[]>;
+
     getAllThreads(limit: number, page: number): Promise<Thread[]>;
+
+    getMessagesPaginated(threadId: number, limit: number, page: number): Promise<Pagination<Message>>;
 }
 
 export class ProdApi implements IApi {
-    public async getAllMessages(threadId: number): Promise<Message[]> {
+    public async getAllMessages(threadId: number): Promise<Message[], PaginationApiModel<MessageApiModel>> {
         return axios.get(`/conversation/${threadId}/messages/desc`)
             .then(r => MessageConverter.convertMessages(r.data.items));
     }
@@ -19,10 +30,26 @@ export class ProdApi implements IApi {
         return axios.get(`/conversation/all?limit=${limit}&page=${page}`)
             .then(r => ThreadConverter.convertThreads(r.data.items));
     }
+
+    public async getMessagesPaginated(threadId: number, limit: number, page: number): Promise<Pagination<Message>> {
+        return axios.get(`/conversation/${threadId}/messages/desc?limit=${limit}&page=${page}`)
+            .then(resp => {
+                const paginationApiModel = resp.data as PaginationApiModel<MessageApiModel>;
+
+                return {
+                    items: MessageConverter.convertMessages(paginationApiModel.items),
+                    perPage: paginationApiModel.per_page,
+                    page: paginationApiModel.page,
+                    total: paginationApiModel.total
+                } as Pagination<Message>;
+            })
+    }
 }
 
 type DevApiDataT = {
-    threads: (Thread & {messages: Message[]})[];
+    threads: (Thread & { messages: Message[] })[];
+
+    generateMessages(senderNames: string[], max: number): Message[];
 }
 
 const DevApiData: DevApiDataT = {
@@ -90,15 +117,41 @@ const DevApiData: DevApiDataT = {
             messages: []
         }
     ],
+
+    generateMessages(senderNames: string[], max: number): Message[] {
+        const result: Message[] = [];
+
+        for (let x = 0; x < max; x++) {
+            const message: Message = {
+                id: x,
+                attachments: [],
+                type: "generic",
+                senderName: senderNames[Math.floor(Math.random() * senderNames.length)],
+                sentAt: new Date(2023, 1, Math.floor(Math.random() * 28), Math.floor(Math.random() * 23), Math.floor(Math.random() * 59)),
+                content: (Math.random() + 1).toString(36).substring(7),
+                callDuration: null
+            }
+
+            result.push(message);
+        }
+
+        return result.sort((msgA, msgB) => new Date(msgA.sentAt) - new Date(msgB.sentAt));
+    }
 }
 
-export class DevApi implements IApi{
+export class DevApi implements IApi {
     public async getAllMessages(threadId: number): Promise<Message[]> {
         return DevApiData.threads.find(x => x.id == threadId)?.messages ?? [];
     }
 
     public async getAllThreads(limit: number, page: number): Promise<Thread[]> {
         return Promise.resolve(DevApiData.threads);
+    }
+
+    getMessagesPaginated(threadId: number, limit: number, page: number): Promise<Pagination<Message>> {
+        const senderNames = ["Kacper Faber", "Karolinka Karolina", "Kamil Kamil", "Adam Adam"];
+        const messages = DevApiData.generateMessages(senderNames, limit)
+        return Promise.resolve({total: 1000, page, perPage: limit, items: messages});
     }
 }
 
